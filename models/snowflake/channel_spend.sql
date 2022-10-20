9b229f4f5f1585fc09e2376cdcbbe63f7896ae7b
zloff <zoe@poplindata.com>
2022-10-20 12:48:53 +1100

    Create channel spend as a macro to enable use with dispatch

--- models/snowflake/channel_spend.sql
+++ models/snowflake/channel_spend.sql
@@ -7,16 +7,9 @@
 -- Note that all flags are passed into this template (e.g. conversion_window_start/end_date).
 
 
--- TODO: put in your own spend calculations per channel here
--- the model below assigns an example 10k spend to each channel
+-- TODO: put in your own spend calculations per channel in the channel_spend macro in your own dbt project
+-- the model assigns an example 10k spend to each channel
 -- found in channel_counts
 
-WITH channels AS (
-    SELECT ARRAY_AGG(DISTINCT channel) AS c FROM {{ ref('channel_counts') }}
-)
-SELECT
-CAST(channel.value AS STRING) AS channel,
-10000 AS spend
-FROM
-channels,
-LATERAL FLATTEN(c) channel
+
+{{ channel_spend() }}
